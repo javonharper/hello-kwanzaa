7e5ba15a8d17d3392fba0ea3192e444fe77092e3
Javon Harper <javon.d.harper@gmail.com>
2019-04-07 16:25:14 -0400

    Add entrypoint to faq

---
+++ src/faq.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    question: 'here we gooooo',
+    answer: 'this is the answer'
+  },
+  {
+    question: 'here we gooooo',
+    answer: 'this is the answer'
+  },
+  {
+    question: 'here we gooooo',
+    answer: 'this is the answer'
+  },
+  {
+    question: 'here we gooooo',
+    answer: 'this is the answer'
+  },
+];
