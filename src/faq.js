864296ece7f21ff0407e18e3d9724a81547348fe
tiffanydrobinson <48872172+tiffanydrobinson@users.noreply.github.com>
2019-04-07 16:46:11 -0400

    Update faq.js

--- src/faq.js
+++ src/faq.js
@@ -1,7 +1,16 @@
 export default [
   {
-    question: 'here we gooooo',
-    answer: 'this is the answer'
+    question: 'how do you decorate your home with the symbols of Kwanzaa?',
+    answer: '- Kinara or candle holder
+- 7 candles, 3 red, 3 green, and 1 black (for each day of kwanzaa)
+- cloth or mat to set kinara on
+- ear of corn for each child in the household
+- unity cup: mkeka
+- put aafrican food and crops, yams, peanut and other food that symbolized african heritage
+
+These are the recommended items needed. IF you do not have the specific item, alternatives can be used.
+i.e., if no kinara, use candle holders; if you are unable to obtain the specific colors for each candle, use any candle as long you have 7 total candles to represent each day
+'
   },
   {
     question: 'here we gooooo',
