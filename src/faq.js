38bf013fefcdbe022b77bf0562c795bfd15ef99d
tiffanydrobinson <48872172+tiffanydrobinson@users.noreply.github.com>
2019-04-10 19:29:46 -0400

    Update faq.js

--- src/faq.js
+++ src/faq.js
@@ -13,15 +13,31 @@ i.e., if no kinara, use candle holders; if you are unable to obtain the specific
 `
   },
   {
-    question: 'here we gooooo',
+    question: 'How do you celebrate Kwanzaa?',
+    answer: `- start at sundown
+- gather around kinara
+- light the candle based on the day
+- drink from the unity cup
+- any leftover liquid is poured over a live crop
+- discuss the principle of the day
+- recommendation: fast from sun up to sun down to honor the ancestors'`
+  },
+  {
+    question: 'How do you light the kinara?',
     answer: 'this is the answer'
   },
   {
-    question: 'here we gooooo',
-    answer: 'this is the answer'
+    question: 'What is Kwanzaa?',
+    answer: `- what is it
+- who started it
+- when
+- who celebrates it: 
+
+- celebrated around the world
+- runs from dec 26th to jan 1`
   },
   {
-    question: 'here we gooooo',
-    answer: 'this is the answer'
+    question: 'Does this replace Christmas?',
+    answer: `No. It is a cultural holiday and can be celebrated in addition to Christmas. The first day of kwanzaa starts on Dec. 26, the day after Christmas and ends on January 1.`
   },
 ];
